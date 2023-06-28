2b6d0cce25806d3de1c20a37ed61d5b048afcfbd
MikeVale16 <mikevale45130@gmail.com>
2023-06-27 18:54:33 -0600

    Add ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,9 @@
+
+#Escribe un script que solicite un número (n) y calcule el valor de n + nn + nnn
+
+
+numero= int(input("Dame un numero"))
+
+resultado= numero + numero*2 + numero*3 
+
+print("El resultado es: ", resultado)
