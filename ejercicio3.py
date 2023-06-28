1f8f9d340ffd24ba4aed7e7546807a5f0eeb6fa5
MikeVale16 <mikevale45130@gmail.com>
2023-06-27 18:50:12 -0600

    Add ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+#3. Escribe un script que calcule el área de un círculo basado en el radio proporcionado por el usuario e imprima el resultado.#
+
+
+radio= float(input("Dame el radio de tu circulo"))
+
+area= 3.1415*radio
+
+print("El radio de tu figura es: ",area)
