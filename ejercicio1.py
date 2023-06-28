1e37033fa11cc12f787f57751e299593ee37cb68
MikeVale16 <mikevale45130@gmail.com>
2023-06-27 18:32:06 -0600

    initial commit

---
+++ ejercicio1.py
@@ -0,0 +1,3 @@
+#ejercicio 1
+
+print ("A la víbora, víbora\n\tDe la mar, de la mar\nPor aquí pueden pasar\nLos de adelante corren mucho\n\tY los de atrás se quedarán\n\n\nA la víbora, víbora\n\tDe la mar, de la mar\nPor aquí pueden pasar\nLos de adelante corren mucho\n\tY los de atrás se quedarán\n\t\tTras, tras, tras")
