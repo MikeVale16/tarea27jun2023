f08a5fb0167bc65cf8b773cdcd268e1e704dec1e
MikeVale16 <mikevale45130@gmail.com>
2023-06-27 19:00:59 -0600

    Add ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+#6. Escribe un script que solicite la base y la altura de un triángulo, calcule su área e imprima el resultado
+
+base= int(input("Dame la base de tu triangulo: \n"))
+altura= int(input("Dame la base de tu triangulo: \n"))
+
+resultado= (base*altura)/2
+
+print ("El resultado es: ", resultado)
